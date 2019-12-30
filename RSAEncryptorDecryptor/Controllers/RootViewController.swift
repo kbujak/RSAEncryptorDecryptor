d487d74f2b307c7b35018ad4600f428f8cd0c7c5
Krystian Bujak <Booyac@MacBook-Pro-macbook.local>
2019-12-30 18:49:17 +0100

    Changed SwiftUI for programming UI

---
+++ RSAEncryptorDecryptor/Controllers/RootViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RootViewController.swift
+//  RSAEncryptorDecryptor
+//
+//  Created by Krystian Bujak on 30/12/2019.
+//  Copyright © 2019 Booyac IT. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
